c89be72a45c62a017ea96dd5845e879c8015d9e7
Emma Parsley <emmaparsley98@gmail.com>
2024-12-04 20:56:50 +0000

    a Knife!

--- SecretSantaGameUnity/Assets/Scripts/Player/PlayerMovement.cs
+++ SecretSantaGameUnity/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,8 @@ namespace SecretSanta.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        private Vector2 _startFingerPos;
         [SerializeField]
         private float _speed;
-
-        private bool _touching;
 
         private PlayerControls playerControls;
 
@@ -38,45 +35,6 @@ namespace SecretSanta.Player
             var pos = transform.position;
             pos += new Vector3(velocity.x, velocity.y, 0);
             transform.position = pos;
-        }
-
-        private Vector2 CheckTouchControls()
-        {
-            var touch = Input.GetTouch(0);
-
-            if (!_touching)
-            {
-                _startFingerPos = touch.position;
-                _touching = true;
-            }
-
-            var dir = touch.position - _startFingerPos;
-
-            return dir.normalized;
-        }
-
-        private Vector2 CheckKeyboardControls()
-        {
-            var dir = new Vector2();
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                dir.y += 1;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                dir.y -= 1;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                dir.x -= 1;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                dir.x += 1;
-            }
-
-            return dir;
         }
     }
 }
---
+++ SecretSantaGameUnity/Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SecretSanta.Player
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] int _damage = 1;
+        [SerializeField] float _speed = 2;
+        [SerializeField] float _cooldown = 0.5f;
+
+        bool _swinging = false;
+        bool _isClockwise = true;
+        float _timeCooling = 0;
+
+        private void Update()
+        {
+            if (_swinging)
+            {
+                return;
+            }
+
+            if (_timeCooling >= _cooldown)
+            {
+                StartCoroutine(WaitForSwing());
+                _timeCooling = 0;
+                return;
+            }
+
+            _timeCooling += Time.deltaTime;
+
+        }
+
+        private IEnumerator<YieldInstruction> WaitForSwing()
+        {
+            _swinging = true;
+
+            float multiplier = _isClockwise ? 1 : -1;
+            _isClockwise = !_isClockwise;
+            float totRotateAmount = 0;
+
+            while (totRotateAmount < 360)
+            {
+                //TODO: This should start fast and slow as it rotates
+                float rotateAmount = 100 * _speed * Time.deltaTime * multiplier;
+                transform.Rotate(new Vector3(0, 0, rotateAmount));
+                totRotateAmount += rotateAmount * multiplier;
+                yield return null;
+            }
+
+            transform.SetLocalPositionAndRotation(transform.localPosition, Quaternion.identity);
+
+            _swinging = false;
+        }
+
+    }
+}
